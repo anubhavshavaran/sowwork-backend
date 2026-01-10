d57bcd989516a6d1610ac5d3e561cf4d39cbb25a
Anubhav <anubhavshavaran@gmail.com>
2026-01-10 14:00:59 +0530

    user data is sent on the successfull code verification

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, HttpCode, HttpStatus, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, UseGuards } from '@nestjs/common';
 import { CurrentUser } from 'src/auth/decorators';
 import { AuthGuard } from 'src/guards';
 import { type UserDocument } from './schemas';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto';
 
 @Controller('user')
 export class UserController {
@@ -12,6 +13,13 @@ export class UserController {
   @HttpCode(HttpStatus.OK)
   @UseGuards(AuthGuard)
   getProfile(@CurrentUser() user: UserDocument) {
-    return this.userService.findUser({_id: user?._id}, ['-embedding']);
+    return this.userService.findUser({ _id: user?._id }, ['-embedding']);
+  }
+
+  @Get('update-profile')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard)
+  updateProfile(@CurrentUser() user: UserDocument, @Body() updateProfileDto: CreateUserDto) {
+    return this.userService.updateUser({ _id: user?._id }, updateProfileDto);
   }
 }
